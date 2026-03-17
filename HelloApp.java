71dd71da2913b5548d686f924b0bd9c7d6254c46
mohan <mohanraja7365@gmail.com>
2026-03-17 14:34:13 +0530

    Added UC2 functionality

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,21 @@
+/**
+ * HelloApp.java
+ * A simple Java application that:
+ * UC1: Displays "Hello World"
+ * UC2: Displays user name from command-line argument
+ */
+
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+        // Check if user provided a name
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello World");
+        }
+
+    }
 }
