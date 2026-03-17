195cbfd354036fa51789db66a7592d059f285bb8
mohan <mohanraja7365@gmail.com>
2026-03-17 14:49:35 +0530

    Added UC3 functionality

--- HelloApp.java
+++ HelloApp.java
@@ -1,21 +1,34 @@
 /**
  * HelloApp.java
- * A simple Java application that:
- * UC1: Displays "Hello World"
- * UC2: Displays user name from command-line argument
+ * A simple Java application that greets the user by name if provided,
+ * or defaults to "World" if no name is given.
+ *
+ * UC1: Display "Hello World"
+ * UC2: Display User Name
+ * UC3: Provide Default Value
+ *
+ * Usage: java HelloApp [name]
+ * If a name is provided → "Hello, [Name]!"
+ * If no name → "Hello, World!"
+ *
+ * @author Mohan
+ * @version 3.0
+ * @since UC1
  */
 
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Check if user provided a name
+        // Default value
+        String name = "World";
+
+        // Check if argument is provided
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            System.out.println("Hello World");
+            name = args[0];
         }
 
+        // Print greeting
+        System.out.println("Hello, " + name + "!");
     }
 }
