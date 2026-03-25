53a53428d1f3b32fa4e3f1cd1d0f1c144b675ed7
mohan <mohanraja7365@gmail.com>
2026-03-25 09:08:55 +0530

    Updated default name to Guest (UC7 improvement)

--- HelloApp.java
+++ HelloApp.java
@@ -1,30 +1,17 @@
 /**
  * HelloApp.java
- * UC6: Display  "Hello" with Multiple Command-Line Arguments
+ * UC7: Display  "Hello" with Multiple Command-Line Arguments
  **/
 
 public class HelloApp {
     public static void main(String[] args) {
 
-        String name;
+        String names = "World";
 
         if (args.length > 0) {
-            // Use StringBuilder to efficiently concatenate names
-            StringBuilder nameBuilder = new StringBuilder();
-
-            // Enhanced for loop: iterate over each argument
-            for (String arg : args) {
-                nameBuilder.append(arg).append(", ");
-            }
-
-            // Remove the trailing ", " using substring
-            name = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-        } else {
-            // Default to "World" if no arguments are provided
-            name = "World";
+            names = String.join(", ", args);
         }
 
-        System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + names + "!");
     }
 }
